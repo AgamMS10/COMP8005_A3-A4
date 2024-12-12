82db1b806841e19633be8766cd9a01624a61f6d9
AgamMS10 <44703146+AgamMS10@users.noreply.github.com>
2024-12-12 09:16:40 -0800

    Removed Useless file

--- src/common/CommonInterface.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface CommonInterface extends Remote {
-    void submitResult(String workerId, String taskId, String result) throws RemoteException;
-}
